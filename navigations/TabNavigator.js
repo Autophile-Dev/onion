8e241280039963e6c17eb9a3aba5cca79709ba57
ch-dev-waleed <waleed.syslab@gmail.com>
2025-01-18 22:25:29 +0500

    Changes on 1/18/25

---
+++ navigations/TabNavigator.js
@@ -0,0 +1,9 @@
+import React from "react";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+
+const Tab = createBottomTabNavigator();
+export default function MainNavigator() {
+    <Tab.Navigator>
+        <Tab.Screen  />
+    </Tab.Navigator>
+}
